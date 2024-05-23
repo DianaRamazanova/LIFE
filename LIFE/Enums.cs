5f2078c40ece5d7999c50863eea858e0b93b18ca
Диана Рамазанова <diana.nfx11@gmail.com>
2024-05-24 01:00:50 +0300

    Структура и перечисления

---
+++ LIFE/Enums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIFE
+{
+        public enum EStateCell
+        {
+            Empty,
+            Alive
+        }
+
+        public enum EStateGame
+        {
+            Run,
+            GameOver
+        }
+}
---
+++ LIFE/Point2D.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIFE
+{
+    public struct Point2D
+    {
+        int X, Y;
+
+        public Point2D (int x = 0, int y = 0)
+        {
+            X = x;  
+            Y = y;
+        }
+
+    }
+}
