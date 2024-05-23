a3526d3f21a90dbe777c3f225084b1b03fe8d32c
Диана Рамазанова <diana.nfx11@gmail.com>
2024-05-24 02:02:52 +0300

    игровое поле

---
+++ LIFE/Cell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIFE
+{
+    public class Cell
+    {
+        private EStateCell _state;
+
+        public Cell() { }
+        public Cell(EStateCell state)
+        {
+            _state = state;
+        }
+
+        public EStateCell State // свойство чтоб менять и выводить данные
+        { 
+            get => _state;
+            set => _state = value;
+        }
+         public bool IsAlive()// проверка клетки
+        {
+            return _state == EStateCell.Alive;
+        }
+    }
+}
---
+++ LIFE/GameField.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIFE
+{
+    public class GameField // matrix
+    {
+        private Cell[,] _map;
+        private int _rows;
+        private int _columns;
+
+        public GameField (int rows = 0, int columns = 0)
+        {
+            _rows = rows;   
+            _columns = columns;
+
+            _map = new Cell[rows, columns];
+        }
+        // cameback size of map
+        public int Rows => _rows;
+        public int Columns => _columns; 
+
+        //cameback map for read
+        public Cell[,] Map => _map;
+        
+        public int CountOfLivingCellsArround(Point2D point) // подсчитывает количество живых ячейк вокруг точки
+        {
+
+        }
+        private CountOfLivingCellsRange (int rows_from, int columns_to,
+           int columns_from, int rows_to, Point2D exclude_point);
+
+    }
+}
