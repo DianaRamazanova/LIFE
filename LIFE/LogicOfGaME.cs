26c19de3717ceb81c37d2106ec346a0f18f104de
Диана Рамазанова <diana.nfx11@gmail.com>
2024-05-24 16:02:06 +0300

    начало логики игры

---
+++ LIFE/LogicOfGaME.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIFE
+{
+    public class LogicOfGame
+    {
+        private GameField _gameField;
+        public LogicOfGame(int rows = 10, int columns = 10)
+        {
+            _gameField = new GameField(rows, columns);
+            RandLocationAliveCells();
+        }
+
+        public LogicOfGame(List<Point2D> startLocationAliveCells, int rows = 10, int columns = 10)
+        {
+            _gameField = new GameField(rows, columns);
+        }
+
+        public GameField GetGameField => _gameField;
+        private void RandLocationAliveCells()
+        {
+
+        }
+    }
+}
